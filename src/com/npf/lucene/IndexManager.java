dad33916734e3a71d2c4b56ccb6e9b3606cc97eb
pengfeinie <pfnie@yahoo.com>
2017-02-26 17:18:16 +0800

    ok

--- src/com/npf/lucene/IndexManager.java
+++ src/com/npf/lucene/IndexManager.java
@@ -15,7 +15,6 @@ import org.apache.lucene.index.IndexWriterConfig;
 import org.apache.lucene.store.Directory;
 import org.apache.lucene.store.FSDirectory;
 import org.apache.lucene.util.Version;
-import org.junit.Test;
 
 public class IndexManager {
 	
@@ -25,56 +24,107 @@ public class IndexManager {
 	
 	public static final String indexPath = manager.getResource("com/npf/lucene/index").getPath();
 	
-	@Test
-	public void indexCreate() throws Exception{
-		//创建文档列表,保存多个Document
+	
+	public static void main(String[] args) throws Exception{
+		//1. 创建文档列表,保存多个Document
+		List<Document> docList = createDocumentList();
+		//2.获取源文件所在目录
+		File dir = getSourceFileDirectory(); 
+		//3.遍历源文件所在目录下的所有文件，并将每个具体文件封装成Document,加入Document列表中
+		docList = lookupFileIntoDocument(docList,dir);
+		//4.创建分词器Analyzer
+		Analyzer analyzer = createAnalyzer();
+		//5.指定索引和文档存储的目录
+		Directory directory = createDirectory();
+		//6.创建索引写对象
+		IndexWriter indexWriter = createIndexWriter(analyzer,directory);
+		//7.将文档加入索引写对象
+		addDocumentToIndexWriter(indexWriter,docList);
+		//8.提交和关闭索引写对象
+		indexWriterCommitClose(indexWriter);
+	}
+	
+	/**
+	 * 创建文档列表,保存多个Document
+	 */
+	public static List<Document> createDocumentList(){
 		List<Document> docList = new ArrayList<Document>();
-		
-		//指定文件所在目录
+		return docList;
+	}
+	
+	/**
+	 * 获取源文件所在目录
+	 */
+	public static File getSourceFileDirectory(){
 		File dir = new File(dirPath); 
-		
+		return dir;
+	}
+	
+	/**
+	 * 遍历源文件所在目录下的所有文件，并将每个具体文件封装成Document,加入Document列表中
+	 */
+	public static List<Document> lookupFileIntoDocument(final List<Document> docList,File dir) throws Exception{
 		for(File file : dir.listFiles()){
-			//文件名称
 			String fileName = file.getName();
-			//文件内容
 			String fileContext = FileUtils.readFileToString(file);
-			//文件大小
 			Long fileSize = FileUtils.sizeOf(file);
-			
-			//文档对象,文件系统中的一个文件就是一个Document对象
 			Document doc = new Document();
-			
 			//第一个参数:域名
 			//第二个参数:域值
 			//第三个参数:是否存储,是为yes,不存储为no
 			TextField nameFiled = new TextField("fileName", fileName, Store.YES);
 			TextField contextFiled = new TextField("fileContext", fileContext, Store.YES);
 			TextField sizeFiled = new TextField("fileSize", fileSize.toString(), Store.YES);
-			
 			//将所有的域都存入文档中
 			doc.add(nameFiled);
 			doc.add(contextFiled);
 			doc.add(sizeFiled);
-			
 			docList.add(doc);
-			
-			//创建分词器,StandardAnalyzer标准分词器,标准分词器对英文分词效果很好,对中文是单字分词
-			Analyzer analyzer = new StandardAnalyzer();
-			//指定索引和文档存储的目录
-			Directory directory = FSDirectory.open(new File(indexPath));
-			//创建写对象的初始化对象
-			IndexWriterConfig config = new IndexWriterConfig(Version.LUCENE_4_10_3, analyzer);
-			//创建索引和文档写对象
-			IndexWriter indexWriter = new IndexWriter(directory, config);
-			
-			//将文档加入到索引和文档的写对象中
-			for(Document doc1 : docList){
-				indexWriter.addDocument(doc1);
-			}
-			//提交
-			indexWriter.commit();
-			//关闭流
-			indexWriter.close();
 		}
+		return docList;
+	}
+	
+	/**
+	 * 创建分词器,StandardAnalyzer标准分词器,标准分词器对英文分词效果很好,对中文是单字分词
+	 */
+	public static Analyzer createAnalyzer(){
+		Analyzer analyzer = new StandardAnalyzer();
+		return analyzer;
+	}
+	
+	/**
+	 * 指定索引和文档存储的目录
+	 */
+	public static Directory createDirectory() throws Exception{
+		Directory directory = FSDirectory.open(new File(indexPath));
+		return directory;
+	}
+	
+	/**
+	 * 创建索引写对象
+	 */
+	public static IndexWriter createIndexWriter(Analyzer analyzer,Directory directory) throws Exception{
+		//创建写对象的初始化对象
+		IndexWriterConfig config = new IndexWriterConfig(Version.LUCENE_4_10_3, analyzer);
+		//创建索引和文档写对象
+		IndexWriter indexWriter = new IndexWriter(directory, config);
+		return indexWriter;
+	}
+	
+	/**
+	 * 将文档加入到索引和文档的写对象中
+	 */
+	public static void addDocumentToIndexWriter(IndexWriter indexWriter,List<Document> docList) throws Exception{
+		for(Document doc1 : docList){
+			indexWriter.addDocument(doc1);
+		}
+	}
+	
+	/**
+	 * 索引写对象的提交与关闭
+	 */
+	public static void indexWriterCommitClose(IndexWriter indexWriter) throws Exception{
+		indexWriter.commit();
+		indexWriter.close();
 	}
 }
